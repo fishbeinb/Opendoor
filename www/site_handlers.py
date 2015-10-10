4cfc5582f663ae2c9b55e64313ad15147fbfc9f8
Ben <fishbeinb@gmail.com>
2015-10-10 17:14:03 -0500

    minor remove

--- www/site_handlers.py
+++ www/site_handlers.py
@@ -5,8 +5,8 @@ import www.error.handlers
 
 www_handlers = [
     RedirectRoute('/', handler=www.index.handlers.IndexHandler, name='Index', strict_slash=True),
-    RedirectRoute('/listings', handler=www.index.handlers.ListingsHandler, name='Listings', strict_slash=True),
-    RedirectRoute('/populate', handler=www.index.handlers.PopulateHandler, name='Populate', strict_slash=True)
+    RedirectRoute('/listings', handler=www.index.handlers.ListingsHandler, name='Listings', strict_slash=True)
+    #RedirectRoute('/populate', handler=www.index.handlers.PopulateHandler, name='Populate', strict_slash=True)
 ]
 
 error_handlers = {
